d1fd38da7f7f82d1b70744f0ba5634b1f46a704d
Joshua Thomas <ST10263292@vcconnect.edu.za>
2024-04-16 11:36:21 +0200

    creating the method for user to engter details of meal.

--- Program.cs
+++ Program.cs
@@ -2,61 +2,21 @@
 
 namespace recipe_App
 {
-    public class Recipe
-    {
-        public string Name { get; set; }
-        public string[] Ingredients { get; set; }
-        public string[] Steps { get; set; }
-    }
+
 
     class Program
     {
 
-        public void Menu()
-        {
+
+
+        static void Main(string[] args)
+        { 
             Recipe recipe = new Recipe();
-            Console.WriteLine("Please enter your name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Welcome, " + name + ", to the recipe app");
-            Console.WriteLine("Let's get cooking!");
-
-            while (true)
-            {
-                Console.WriteLine("Choose an option:");
-                Console.WriteLine("1. Enter recipe details");
-                Console.WriteLine("2. Display recipe");
-                Console.WriteLine("3. Scale recipe");
-                Console.WriteLine("4. Reset quantities");
-                Console.WriteLine("5. Clear all data");
-                Console.WriteLine("6. Exit");
-
-                string choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        EnterRecipeDetails(recipe);
-                        break;
-                    case "2":
-                        DisplayRecipe(recipe);
-                        break;
-                    case "3":
-                        Scale(recipe);
-                        break;
-                    case "4":
-                        ResetQuantities(recipe);
-                        break;
-                    case "5":
-                        ClearData(recipe);
-                        break;
-                    case "6":
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        break;
-                }
-            }
+            working working = new working();
+            working.Menu(recipe);
+            working.EnterRecipeDetails(recipe);   
         }
-       
+
+    }
+}
 
--- working.cs
+++ working.cs
@@ -8,13 +8,91 @@ namespace recipe_App
 {
     internal class working
     {
-        public void EnterRecipeDetails()
+        public class Recipe
         {
-
-
+            public string Name { get; set; }
+            public string[] Ingredients { get; set; }
+            public string[] Steps { get; set; }
         }
 
+        public void Menu()
+        {
+            Recipe recipe = new Recipe();
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome, " + name + ", to the recipe app");
+            Console.WriteLine("Let's get cooking!");
 
+            while (true)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Enter recipe details");
+                Console.WriteLine("2. Display recipe");
+                Console.WriteLine("3. Scale recipe");
+                Console.WriteLine("4. Reset quantities");
+                Console.WriteLine("5. Clear all data");
+                Console.WriteLine("6. Exit");
 
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        EnterRecipeDetails(recipe);
+                        break;
+                    case "2":
+                        DisplayRecipe(recipe);
+                        break;
+                    case "3":
+                        Scale(recipe);
+                        break;
+                    case "4":
+                        ResetQuantities(recipe);
+                        break;
+                    case "5":
+                        ClearData(recipe);
+                        break;
+                    case "6":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        public void EnterRecipeDetails(Recipe recipe)
+        {
+            Console.WriteLine("What is the name of your dish?");
+            recipe.Name = Console.ReadLine();
+
+            Console.WriteLine("How many ingredients are there?");
+            int ingredientsCount = int.Parse(Console.ReadLine());
+            recipe.Ingredients = new string[ingredientsCount];
+
+            for (int i = 0; i < ingredientsCount; i++)
+            {
+                Console.WriteLine("Enter ingredient " + (i + 1) + ":");
+                recipe.Ingredients[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("How many steps are there?");
+            int stepsCount = int.Parse(Console.ReadLine());
+            recipe.Steps = new string[stepsCount];
+
+            for (int i = 0; i < stepsCount; i++)
+            {
+                Console.WriteLine("Enter step " + (i + 1) + ":");
+                recipe.Steps[i] = Console.ReadLine();
+            }
+        }
     }
 }
+
+
+
+
+
+
+       
